1b0c0ecfc09ac67abed19f376928155f60a3b0c5
zordq <coresak@hotmail.com>
2021-01-13 17:02:53 +0700

    index update

---
+++ js/move.train.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){const t=new Motus.Animation({startPoint:[-50],endPoint:[-450],$el:document.getElementById("train"),keyframes:[{right:{from:-250,to:30,unit:"px"}}]});Motus.addAnimation(t);const n=new Motus.Animation({startPoint:[-50],endPoint:[-450],$el:document.getElementById("train-2"),keyframes:[{left:{from:-250,to:30,unit:"px"}}]});Motus.addAnimation(n)}));
