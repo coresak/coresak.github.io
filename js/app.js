0a428099eca43d3285d973ed62f0f1ea403fae3a
zordq <coresak@hotmail.com>
2021-01-16 16:00:10 +0700

    index update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-$(document).ready((function(){AOS.init({offset:120,delay:100,easing:"ease",once:!0,mirror:!1,anchorPlacement:"top"}),$(".counter").counterUp({delay:30,time:1e3}),$(".nav__dots-btn").click((function(){$(this).toggleClass("open"),$(".header__list").toggleClass("open")}))}));
+$(document).ready((function(){AOS.init({offset:120,delay:100,easing:"ease",once:!0,mirror:!1,anchorPlacement:"top"}),$(".counter").counterUp({delay:30,time:1500}),$(".nav__dots-btn").click((function(){$(this).toggleClass("open"),$(".header__list").toggleClass("open")}))}));
